b5caf0ef0621ac9a158cbbf4845604bf01298edb
DESKTOP-LBOHS1J\hungnm2 <hung.nguyen@netvisiontel.com>
2020-10-17 15:41:38 +0700

    update

--- wordpress/host_container_copy_file.sh
+++ wordpress/host_container_copy_file.sh
@@ -2,5 +2,6 @@
 # docker cp <source>:<dir> <destination>:<dir>
 # <source>, <destination>: có thể là host or container
 # test-nginx: là domain = hostname container trong docker-network
+
 docker cp  wordpress_wordpress_1:/var/www/html  . 
 
